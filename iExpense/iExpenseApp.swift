8c3205292d965b6373d12e426da69cd31510aef1
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-10-18 09:29:54 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Oliver Delaney on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
